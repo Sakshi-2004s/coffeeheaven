1ef2bf7b104175708970348b2e9b0c8be4a4f2be
Sakshi-2004s <sakshishinde4030@gmail.com>
2026-08-27 13:10:45 +0530

    Update database connection details in db.php

--- db.php
+++ db.php
@@ -1,12 +1,12 @@
-<?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$dbname = "coffee_heaven"; 
+<?php 
+$host = "sql305.infinityfree.com"; 
+$user = "if0_42754885"; 
+$pass = "CoffeeHeaven08"; 
+$dbname = "if0_42754885_coffeeheaven";  
 
-$conn = new mysqli($host, $user, $pass, $dbname);
+$conn = new mysqli($host, $user, $pass, $dbname); 
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+if ($conn->connect_error) { 
+    die("Connection failed: " . $conn->connect_error); 
+} 
 ?>
