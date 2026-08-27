5ee46682ee16ac06acf8d671a062aae4985d610d
Sakshi-2004s <sakshishinde4030@gmail.com>
2026-08-27 13:12:44 +0530

    Simplify logout redirection
    Removed username from logout redirection for security.

--- logout.php
+++ logout.php
@@ -1,8 +1,11 @@
 <?php
+
 session_start();
 
-$username = $_SESSION['username'] ?? '';
 session_unset();
 session_destroy();
-header("Location: signup.php?logout=1&user=" . urlencode($username));
+
+header("Location: signup.php?logout=1");
 exit();
+
+?>
